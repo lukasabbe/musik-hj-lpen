ae84c2f7ef56b3c5c6ebfb158fcf106ea8832518
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-11-14 16:38:51 +0100

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/:bossa', async (req, res) => {
+    let data =  await fetch(`https://bossan.musikhjalpen.se/page-data/${req.params.bossa}/page-data.json`);
+    let json = await data.json();
+    data = await fetch(`https://musikhjalpen-franceska.herokuapp.com/server/fundraisers/${json.result.data.contentfulFundraiser.contentful_id}?fields%5B%5D=amount&fields=prev_amount`)
+    json = await data.json();
+    res.set("Access-Control-Allow-Origin","*").json(json);
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
