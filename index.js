f38224a2d9d4933a285ac2c720e7ade626be268f
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-11-20 22:20:33 +0100

    easy crash problem fixed

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/:bossa', async (req, res) => {
+
+    if(req.params.bossa === null) return res.status(400).json({error: "No bossa id provided"});
+
     let data =  await fetch(`https://bossan.musikhjalpen.se/page-data/${req.params.bossa}/page-data.json`);
     let json = await data.json();
     data = await fetch(`https://musikhjalpen-franceska.herokuapp.com/server/fundraisers/${json.result.data.contentfulFundraiser.contentful_id}?fields%5B%5D=amount&fields=prev_amount`)
