346e1476c1ed50ca64b5f80765304f94f68114bd
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-11-20 22:35:09 +0100

    fixed so it don't crash on no exsisting

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/:bossa', async (req, res) => {
     if(req.params.bossa === null || req.params.bossa == "favicon.ico") return res.status(400).json({error: "No bossa id provided"});
 
     let data = await fetch(`https://bossan.musikhjalpen.se/page-data/${req.params.bossa}/page-data.json`);
+    if(data.status != 200) return res.status(400).json({error: "Invalid bossa id"});
     let json = await data.json().catch();
     data = await fetch(`https://musikhjalpen-franceska.herokuapp.com/server/fundraisers/${json.result.data.contentfulFundraiser.contentful_id}?fields%5B%5D=amount&fields=prev_amount`)
     json = await data.json()
